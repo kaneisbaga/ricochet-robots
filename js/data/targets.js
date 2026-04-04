7b9aebf38a485fd06be5eff668655e04443f94e0
kaneisbaga <67339834+kaneisbaga@users.noreply.github.com>
2026-04-05 01:30:52 +0800

    Delete js directory

--- js/data/targets.js
+++
@@ -1,7 +0,0 @@
-// Ricochet Robots v1.05 | 2026-04-04
-const TARGET_DATA = [
-    { name: '紅星', color: '#ff4444', r: 2, c: 2 },
-    { name: '藍月', color: '#4444ff', r: 3, c: 5 },
-    { name: '綠星', color: '#44bb44', r: 5, c: 1 },
-    { name: '黃太陽', color: '#ffbb00', r: 6, c: 5 }
-];
