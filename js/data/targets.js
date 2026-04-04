c9d68c4db354826bb250c8f68f46567248b0eaad
kaneisbaga <67339834+kaneisbaga@users.noreply.github.com>
2026-04-04 23:21:31 +0800

    Update targets.js

--- js/data/targets.js
+++ js/data/targets.js
@@ -1,7 +1,7 @@
-// Ricochet Robots v1.01 | 2026-04-04
+// Ricochet Robots v1.05 | 2026-04-04
 const TARGET_DATA = [
-    { name: '紅星', color: 'red', r: 2, c: 2 },
-    { name: '藍月', color: 'blue', r: 3, c: 5 },
-    { name: '綠星', color: 'green', r: 5, c: 1 },
-    { name: '黃太陽', color: 'yellow', r: 6, c: 5 }
+    { name: '紅星', color: '#ff4444', r: 2, c: 2 },
+    { name: '藍月', color: '#4444ff', r: 3, c: 5 },
+    { name: '綠星', color: '#44bb44', r: 5, c: 1 },
+    { name: '黃太陽', color: '#ffbb00', r: 6, c: 5 }
 ];
